890c3208875e729038ba039a24c152721ce50b36
aimahusnain <aimahusnain@gmail.com>
2023-12-28 02:20:05 +0500

    package.json2

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -15,5 +15,9 @@ async function extractAllBlogs() {
 export default async function Blogs() {
   const blogPostsList = await extractAllBlogs();
 
-  return <BlogList lists={blogPostsList} />;
+  return (
+    <>
+      <BlogList lists={blogPostsList} />
+    </>
+  );
 }
