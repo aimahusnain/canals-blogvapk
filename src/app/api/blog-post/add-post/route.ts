4c5fc89d994b6b5f6aa46eecf025b98fc00e8bfc
aimahusnain <aimahusnain@gmail.com>
2023-12-28 02:04:02 +0500

    change the api of add-post

--- src/app/api/blog-post/add-post/route.ts
+++ src/app/api/blog-post/add-post/route.ts
@@ -1,32 +1,30 @@
 import prisma from "@/database";
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server"
 
-export async function POST(request: NextRequest) {
+export const POST = async (request: NextRequest) => {
   try {
-    const extractPostData = await request.json();
-    const newlyCreatedPost = await prisma.post.create({
-      data: extractPostData
-    });
-    console.log(extractPostData, 'extractPostData');
+    const getPostData = await request.json()
 
-    if (newlyCreatedPost) {
+    const createPost = await prisma.post.create({
+      data: getPostData
+    })
+
+    if (createPost) {
       return NextResponse.json({
         success: true,
-        message: "New blog post added successfully",
-      });
+        message: 'New blog post created successfully!'
+      })
     } else {
       return NextResponse.json({
         success: false,
-        message: "Something went wrong ! Please try again",
-      });
+        message: 'Something went wrong, please try again!'
+      })
     }
-
-  } catch (e) {
-    console.log(e);
-
+  } catch (error) {
+    console.log(error)
     return NextResponse.json({
       success: false,
-      message: "Something went wrong ! Please try again",
-    });
+      message: 'Something went wrong, please try again!'
+    })
   }
 }
